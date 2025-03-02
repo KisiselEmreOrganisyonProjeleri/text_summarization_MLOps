b5ba01044acc360aaca64adb327985a4ab540189
yunusemrekayis <104589120+yunusemrekayis@users.noreply.github.com>
2025-03-02 09:36:20 +0000

    data ingestion ve validation tamamlandı

--- src/Text_Summarizer/config/configration.py
+++ src/Text_Summarizer/config/configration.py
@@ -1,6 +1,6 @@
 from src.Text_Summarizer.constants import * # constanst icerisinde degiskenleri import ettik
 from src.Text_Summarizer.utils.common import read_yaml, create_directories # common.py icerisinde read_yaml ve create_directories methodlarini import ettik
-from src.Text_Summarizer.entity.config_entity import DataIngestionConfig
+from src.Text_Summarizer.entity.config_entity import DataIngestionConfig, DataValidationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -28,3 +28,14 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir= config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILE=config.ALL_REQUIRED_FILE
+        )
+        return data_validation_config
